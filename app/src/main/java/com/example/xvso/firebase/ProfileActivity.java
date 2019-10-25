4ddb31fa8ed39044697bcbadf38c3ab2e27d63cf
andreeagorcsa <andreea.gorcsa@t-systems.com>
2019-10-25 20:48:06 +0200

    25.10.2019 FirebaseDatabase..

--- app/src/main/java/com/example/xvso/firebase/ProfileActivity.java
+++ app/src/main/java/com/example/xvso/firebase/ProfileActivity.java
@@ -147,7 +147,7 @@ public class ProfileActivity extends BaseActivity implements View.OnClickListene
     @Override
     protected void onResume() {
         super.onResume();
-        readFromDatabase();
+        //readFromDatabase();
     }
 
     private void readFromDatabase() {
@@ -187,19 +187,28 @@ public class ProfileActivity extends BaseActivity implements View.OnClickListene
         lastName = profileBinding.lastNameEditview.getText().toString();
         email = profileBinding.emailEditview.getText().toString();
 
-        UserProfileChangeRequest profileUpdates = new UserProfileChangeRequest.Builder()
-                .setDisplayName(firstName + " " + lastName)
-                .build();
+         if (firstName.isEmpty()) {
+           showMessage("Please introduce your first name");
+         } else if (lastName.isEmpty()) {
+             showMessage("Please introduce your last name");
+         } else if (email.isEmpty()) {
+             showMessage("Please introduce your e-mail address");
+         } else {
 
-        user.updateProfile(profileUpdates)
-                .addOnCompleteListener(new OnCompleteListener<Void>() {
-                    @Override
-                    public void onComplete(@NonNull Task<Void> task) {
-                        if (task.isSuccessful()) {
-                            Log.d(LOG_TAG, "User profile updated.");
-                        }
-                    }
-                });
+             UserProfileChangeRequest profileUpdates = new UserProfileChangeRequest.Builder()
+                     .setDisplayName(firstName + " " + lastName)
+                     .build();
+
+             user.updateProfile(profileUpdates)
+                     .addOnCompleteListener(new OnCompleteListener<Void>() {
+                         @Override
+                         public void onComplete(@NonNull Task<Void> task) {
+                             if (task.isSuccessful()) {
+                                 Log.d(LOG_TAG, "User profile updated.");
+                             }
+                         }
+                     });
+         }
     }
 
 
