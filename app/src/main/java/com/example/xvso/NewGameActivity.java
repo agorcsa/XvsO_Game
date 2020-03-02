0c86724870ff25a9c2ec06b282784006866b4b1b
andreeagorcsa <andreea.gorcsa@t-systems.com>
2020-03-02 18:58:59 +0100

    02.03.2020.
    New games can be added and removed from the list.
    
    TO DO: replace the dummy code with real games from Firebase.

--- app/src/main/java/com/example/xvso/NewGameActivity.java
+++ app/src/main/java/com/example/xvso/NewGameActivity.java
@@ -1,31 +1,94 @@
 package com.example.xvso;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.databinding.DataBindingUtil;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import com.example.xvso.Objects.GameItem;
-import com.example.xvso.databinding.ActivityNewGameBinding;
+import com.example.xvso.adapter.GameAdapter;
 
 import java.util.ArrayList;
 
 public class NewGameActivity extends AppCompatActivity {
 
-    private ActivityNewGameBinding newGameBinding;
+    private ArrayList<GameItem> mOpenGamesList;
+
+    private RecyclerView recyclerView;
+    private RecyclerView.LayoutManager layoutManager;
+    private RecyclerView.Adapter adapter;
+
+    private Button newGameButton;
+    private EditText insertGameEditText;
+    private EditText removeGameEditText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_new_game);
 
-        newGameBinding = DataBindingUtil.setContentView(this, R.layout.activity_new_game);
+        createGameList();
+        buildRecyclerView();
 
-        ArrayList<GameItem> openGames = new ArrayList<>();
+        newGameButton = findViewById(R.id.new_game);
+        insertGameEditText = findViewById(R.id.insert_game_edit_text);
+        removeGameEditText = findViewById(R.id.remove_game_edit_text);
+    }
 
+
+    public void createGameList() {
         // add the games from Firebase
-        // placeholder code
-        openGames.add(new GameItem(R.drawable.ic_zero, "Game 1" , "user 1"));
-        openGames.add(new GameItem(R.drawable.ic_zero, "Game 2" , "user 2"));
-        openGames.add(new GameItem(R.drawable.ic_zero, "Game 3" , "user 3"));
+        // placeholder/dummy code
+        mOpenGamesList = new ArrayList<>();
+
+        mOpenGamesList.add(new GameItem(R.drawable.profile, "Game 1" , "user 1"));
+        mOpenGamesList.add(new GameItem(R.drawable.profile, "Game 2" , "user 2"));
+        mOpenGamesList.add(new GameItem(R.drawable.profile, "Game 3" , "user 3"));
+        mOpenGamesList.add(new GameItem(R.drawable.profile, "Game 4" , "user 4"));
+        mOpenGamesList.add(new GameItem(R.drawable.profile, "Game 5" , "user 5"));
+        mOpenGamesList.add(new GameItem(R.drawable.profile, "Game 6" , "user 6"));
+        mOpenGamesList.add(new GameItem(R.drawable.profile, "Game 7" , "user 7"));
+        mOpenGamesList.add(new GameItem(R.drawable.profile, "Game 8" , "user 8"));
+        mOpenGamesList.add(new GameItem(R.drawable.profile, "Game 9" , "user 9"));
+        mOpenGamesList.add(new GameItem(R.drawable.profile, "Game 10" , "user 10"));
+    }
+
+    public void buildRecyclerView() {
+        recyclerView = findViewById(R.id.games_recycler_view);
+        recyclerView.setHasFixedSize(true);
+        layoutManager = new LinearLayoutManager(this);
+        adapter = new GameAdapter(mOpenGamesList);
+
+        recyclerView.setLayoutManager(layoutManager);
+        recyclerView.setAdapter(adapter);
+    }
+
+    public void onNewGameButtonClicked(View view) {
+        Toast.makeText(getApplicationContext(), "A new game has been created", Toast.LENGTH_LONG).show();
+        int position = Integer.parseInt(insertGameEditText.getText().toString());
+        addNewGame(position);
+    }
+
+    public void addNewGame(int position) {
+        mOpenGamesList.add(position, new GameItem(R.drawable.ic_cross, "A new game has been added at position" + position, "Opponent User Name"));
+        adapter.notifyDataSetChanged();
+    }
+
+    public void removeGameFromList(View view) {
+        Toast.makeText(getApplicationContext(), "The game has been removed", Toast.LENGTH_LONG).show();
+        int position = Integer.parseInt(insertGameEditText.getText().toString());
+        removeGame(position);
+    }
+
+    public void removeGame(int position) {
+        mOpenGamesList.remove(position);
+        adapter.notifyDataSetChanged();
     }
 }
+
+
---
+++ app/src/main/java/com/example/xvso/adapter/GameAdapter.java
@@ -0,0 +1,60 @@
+package com.example.xvso.adapter;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.example.xvso.Objects.GameItem;
+import com.example.xvso.R;
+
+import java.util.ArrayList;
+
+public class GameAdapter extends RecyclerView.Adapter<GameAdapter.GameViewHolder> {
+
+    private ArrayList<GameItem>  mGameItemsList;
+
+    @NonNull
+    @Override
+    public GameViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.game_item, parent, false);
+        GameViewHolder gameViewHolder = new GameViewHolder(view);
+        return gameViewHolder;
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull GameViewHolder holder, int position) {
+        GameItem currentItem = mGameItemsList.get(position);
+        holder.profilePicture.setImageResource(currentItem.getProfilePicture());
+        holder.gameNumber.setText(currentItem.getGameNumber());
+        holder.opponentUserName.setText(currentItem.getLoggedUserName());
+    }
+
+    @Override
+    public int getItemCount() {
+        return mGameItemsList.size();
+    }
+
+    public GameAdapter(ArrayList<GameItem> gameItemsList) {
+           mGameItemsList = gameItemsList;
+    }
+
+    public static class GameViewHolder extends RecyclerView.ViewHolder {
+
+        public ImageView profilePicture;
+        public TextView gameNumber;
+        public TextView opponentUserName;
+
+        public GameViewHolder(@NonNull View itemView) {
+            super(itemView);
+
+            profilePicture = itemView.findViewById(R.id.profile_image_view);
+            gameNumber = itemView.findViewById(R.id.first_line_text_view);
+            opponentUserName = itemView.findViewById(R.id.second_line_text_view);
+        }
+    }
+}
